0d1495ebba2a624f7db37249efaf6c0a5a2a577d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-14 21:00:49 +0100

    feat: configure project

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [
+      {
+        loader: "next/font/google",
+        options: { subsets: ["latin", "latin-ext"] },
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
