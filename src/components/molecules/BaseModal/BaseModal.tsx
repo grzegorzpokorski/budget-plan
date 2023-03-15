34cc2758c3a8a1cd19e647feaaa9818c8b6da37c
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-15 23:53:05 +0100

    feat: add animation on reveal modal

--- src/components/molecules/BaseModal/BaseModal.tsx
+++ src/components/molecules/BaseModal/BaseModal.tsx
@@ -17,14 +17,14 @@ export const BaseModal = ({ title, children, closeModal }: Props) => {
     <div
       id={id}
       role="dialog"
-      className="fixed top-0 left-0 h-full w-full p-3 z-50 overflow-y-auto bg-black/50"
+      className="fixed top-0 left-0 h-full w-full p-3 z-50 overflow-y-auto bg-black/60 animate-[opacity_.15s_ease-in-out]"
       onClick={() => closeModal()}
       aria-modal
       aria-labelledby={`${id}-title`}
     >
       <div className="m-auto flex min-h-full w-full items-center">
         <div
-          className="relative flex flex-col p-6 mx-auto max-w-xl w-full bg-white rounded-md overflow-auto shadow-lg"
+          className="relative flex flex-col p-6 mx-auto max-w-xl w-full bg-white rounded-md overflow-auto shadow-lg animate-[scale_.15s_ease-in-out]"
           onClick={(e) => e.stopPropagation()}
         >
           <div className="flex flex-row justify-between border-b-2 pb-3 mb-3">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-poppins)", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        opacity: {
+          "0%": { opacity: 0.5 },
+          "100%": { opacity: 1 },
+        },
+        scale: {
+          "0%": { transform: "scale(.95)", opacity: 0.5 },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
