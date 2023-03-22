8e94ea50c967a2c0654bc77a667291c2222b6887
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-22 19:55:50 +0100

    refactor: move home page components composition into template

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -1,25 +1,5 @@
-"use client";
-
-import { Container } from "@/components/atoms/Container/Container";
-import { Private } from "@/components/atoms/Private/Private";
-import { BudgetRealization } from "@/components/molecules/BudgetRealization/BudgetRealization";
-import { MainHeader } from "@/components/molecules/MainHeader/MainHeader";
-import { Expenses } from "@/components/organisms/Expenses/Expenses";
-import { Budgets } from "@/components/organisms/Budgets/Budgets";
-import { Toaster } from "sonner";
-import { Modals } from "@/components/organisms/Modals/Modals";
+import { HomePage } from "@/components/templates/HomePage/HomePage";
 
 export default function Home() {
-  return (
-    <Private>
-      <MainHeader />
-      <Container className="flex flex-col py-8">
-        <BudgetRealization name="Realizacja" />
-        <Budgets />
-        <Expenses />
-      </Container>
-      <Modals />
-      <Toaster position="top-center" />
-    </Private>
-  );
+  return <HomePage />;
 }
---
+++ src/components/templates/HomePage/HomePage.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Container } from "@/components/atoms/Container/Container";
+import { Private } from "@/components/atoms/Private/Private";
+import { BudgetRealization } from "@/components/molecules/BudgetRealization/BudgetRealization";
+import { MainHeader } from "@/components/molecules/MainHeader/MainHeader";
+import { Budgets } from "@/components/organisms/Budgets/Budgets";
+import { Expenses } from "@/components/organisms/Expenses/Expenses";
+import { Modals } from "@/components/organisms/Modals/Modals";
+
+export const HomePage = () => (
+  <Private>
+    <MainHeader />
+    <Container className="flex flex-col py-8">
+      <BudgetRealization name="Realizacja" />
+      <Budgets />
+      <Expenses />
+    </Container>
+    <Modals />
+  </Private>
+);
