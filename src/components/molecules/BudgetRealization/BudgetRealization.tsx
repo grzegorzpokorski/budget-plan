e9647a0733c3e8b9280ca6f212875bf05c476f85
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-29 22:33:05 +0200

    refactor: refactor BudgetRealization grid

--- src/components/molecules/BudgetRealization/BudgetRealization.tsx
+++ src/components/molecules/BudgetRealization/BudgetRealization.tsx
@@ -24,23 +24,55 @@ export const BudgetRealization = () => {
   }
 
   return (
-    <section className="flex flex-col md:flex-row gap-4 items-center justify-center">
+    // <section className="flex flex-row flex-wrap gap-4 items-center justify-center">
+    //   <header className="sr-only">
+    //     <h2>Podsumowanie</h2>
+    //   </header>
+    //   <div className="flex flex-row gap-4 w-full">
+    //     <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/2">
+    //       <h3 className="font-bold">Suma wydatków</h3>
+    //       <p className="font-medium text-red-500">
+    //         -{formatCurrency(sumOfExpenses)}
+    //       </p>
+    //     </div>
+    //     <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/2">
+    //       <h3 className="font-bold">Suma zysków</h3>
+    //       <p className="font-medium text-green-500">
+    //         {formatCurrency(sumOfProfits)}
+    //       </p>
+    //     </div>
+    //   </div>
+    //   <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3">
+    //     <h3 className="font-bold">Podsumowanie</h3>
+    //     <p
+    //       className={twMerge(
+    //         "font-medium",
+    //         sumOfProfits - sumOfExpenses >= 0
+    //           ? "text-green-500"
+    //           : "text-red-500",
+    //       )}
+    //     >
+    //       {formatCurrency(sumOfProfits - sumOfExpenses)}
+    //     </p>
+    //   </div>
+    // </section>
+    <section className="grid grid-cols-2 md:grid-cols-3 gap-4">
       <header className="sr-only">
         <h2>Podsumowanie</h2>
       </header>
-      <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3">
+      <div className="flex flex-col gap-2 items-center text-center justify-center p-6 bg-white rounded shadow">
         <h3 className="font-bold">Suma wydatków</h3>
         <p className="font-medium text-red-500">
           -{formatCurrency(sumOfExpenses)}
         </p>
       </div>
-      <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3">
+      <div className="flex flex-col gap-2 items-center text-center justify-center p-6 bg-white rounded shadow">
         <h3 className="font-bold">Suma zysków</h3>
         <p className="font-medium text-green-500">
           {formatCurrency(sumOfProfits)}
         </p>
       </div>
-      <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3">
+      <div className="col-span-2 md:col-span-1 flex flex-col gap-2 items-center text-center justify-center p-6 bg-white rounded shadow">
         <h3 className="font-bold">Podsumowanie</h3>
         <p
           className={twMerge(
--- src/components/molecules/BudgetRealization/Loader.tsx
+++ src/components/molecules/BudgetRealization/Loader.tsx
@@ -1,17 +1,17 @@
 export const Loader = () => (
   <div
-    className="flex flex-col md:flex-row gap-4 items-center justify-center animate-pulse"
+    className="grid grid-cols-2 md:grid-cols-3 gap-4 animate-pulse"
     role="status"
   >
-    <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3 h-[104px]">
+    <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow">
       <div className="h-6 w-36 bg-neutral-200"></div>
       <div className="h-6 w-20 bg-neutral-200"></div>
     </div>
-    <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3 h-[104px]">
+    <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow">
       <div className="h-6 w-36 bg-neutral-200"></div>
       <div className="h-6 w-20 bg-neutral-200"></div>
     </div>
-    <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3 h-[104px]">
+    <div className="col-span-2 md:col-span-1 flex flex-col gap-2 items-center p-6 bg-white rounded shadow">
       <div className="h-6 w-36 bg-neutral-200"></div>
       <div className="h-6 w-20 bg-neutral-200"></div>
     </div>
