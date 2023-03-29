3e31f20a45fb286771b4e7a8fbeed22b78e997b6
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-29 23:07:25 +0200

    refactor: adjust BudgetRealization boxes sizes

--- src/components/molecules/BudgetRealization/BudgetRealization.tsx
+++ src/components/molecules/BudgetRealization/BudgetRealization.tsx
@@ -24,23 +24,23 @@ export const BudgetRealization = () => {
   }
 
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <header className="sr-only">
         <h2>Podsumowanie</h2>
       </header>
-      <div className="flex flex-col gap-2 items-center text-center justify-center p-6 bg-white rounded shadow">
+      <div className="col-span-2 xs:col-span-1 flex flex-row sm:flex-col flex-wrap gap-2 items-center text-center justify-between p-6 bg-white rounded shadow">
         <h3 className="font-bold">Suma wydatków</h3>
         <p className="font-medium text-red-500">
           -{formatCurrency(sumOfExpenses)}
         </p>
       </div>
-      <div className="flex flex-col gap-2 items-center text-center justify-center p-6 bg-white rounded shadow">
+      <div className="col-span-2 sm:col-span-1 flex flex-row sm:flex-col flex-wrap gap-2 items-center text-center justify-between p-6 bg-white rounded shadow">
         <h3 className="font-bold">Suma zysków</h3>
         <p className="font-medium text-green-500">
           {formatCurrency(sumOfProfits)}
         </p>
       </div>
-      <div className="col-span-2 md:col-span-1 flex flex-col gap-2 items-center text-center justify-center p-6 bg-white rounded shadow">
+      <div className="col-span-2 sm:col-span-1 flex flex-row sm:flex-col flex-wrap gap-2 items-center text-center justify-between p-6 bg-white rounded shadow">
         <h3 className="font-bold">Podsumowanie</h3>
         <p
           className={twMerge(
