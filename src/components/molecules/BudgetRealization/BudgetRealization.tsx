384907decb58e307efbadbda5361cc94235ab9b2
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-29 22:34:08 +0200

    fix: remove comments

--- src/components/molecules/BudgetRealization/BudgetRealization.tsx
+++ src/components/molecules/BudgetRealization/BudgetRealization.tsx
@@ -24,38 +24,6 @@ export const BudgetRealization = () => {
   }
 
   return (
-    // <section className="flex flex-row flex-wrap gap-4 items-center justify-center">
-    //   <header className="sr-only">
-    //     <h2>Podsumowanie</h2>
-    //   </header>
-    //   <div className="flex flex-row gap-4 w-full">
-    //     <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/2">
-    //       <h3 className="font-bold">Suma wydatków</h3>
-    //       <p className="font-medium text-red-500">
-    //         -{formatCurrency(sumOfExpenses)}
-    //       </p>
-    //     </div>
-    //     <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/2">
-    //       <h3 className="font-bold">Suma zysków</h3>
-    //       <p className="font-medium text-green-500">
-    //         {formatCurrency(sumOfProfits)}
-    //       </p>
-    //     </div>
-    //   </div>
-    //   <div className="flex flex-col gap-2 items-center p-6 bg-white rounded shadow w-full md:w-1/3">
-    //     <h3 className="font-bold">Podsumowanie</h3>
-    //     <p
-    //       className={twMerge(
-    //         "font-medium",
-    //         sumOfProfits - sumOfExpenses >= 0
-    //           ? "text-green-500"
-    //           : "text-red-500",
-    //       )}
-    //     >
-    //       {formatCurrency(sumOfProfits - sumOfExpenses)}
-    //     </p>
-    //   </div>
-    // </section>
     <section className="grid grid-cols-2 md:grid-cols-3 gap-4">
       <header className="sr-only">
         <h2>Podsumowanie</h2>
